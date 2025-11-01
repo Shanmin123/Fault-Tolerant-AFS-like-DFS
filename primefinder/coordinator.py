e8004479ddae33d4698a0fe1772ad581837c6776
Jiaye39 <liujiaye39@gmail.com>
2025-11-01 01:25:21 +0000

    Refactor coordinator for improved data handling
    Refactor coordinator to use send_data and recv_data functions for communication with workers.

--- primefinder/coordinator.py
+++ primefinder/coordinator.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 import math
+import send_data, recv_data
 
 
 HOST = 'localhost'
@@ -17,33 +18,41 @@ def split_numbers(num, n_workers):
     return [num[i:i+chunk_size] for i in range(0, len(num), chunk_size)]
 
 def coordinator_main():
+    #read data and separate as chunks
     numbers = read_numbers("data/test1.txt")
     chunks = split_numbers(numbers, NUM_workers)
     primes = set()
 
+    #start server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen()
 
     print(f"Coordinator started on {HOST}:{PORT}, waiting for {NUM_workers} workers...")
 
+    #connect with worker
     workers = []
     while len(workers) < NUM_workers:
         conn, addr = server.accept()
         print(f"Worker connected from {addr}")
         workers.append(conn)
-
+        
+    #deliver data
     for i, conn in enumerate(workers):
-        task = pickle.dumps(chunks[i])
-        conn.sendall(task)
+        send_data(conn, chunks[i])
 
-    # 接收结果
-    for conn in workers:
-        data = conn.recv(4096)
-        result = pickle.loads(data)
-        primes.update(result)
-        conn.close()
-
+    #receive results
+    for i, conn in enumerate(workers):
+        print(f'Receiving from worker{i + 1}')
+        try:
+            result= recv_data(conn)
+            if result:
+                primes.update(result)
+        except Exception as e:
+            print(f'Error receiving from worker{i+1}: {e}')
+        finally:
+            conn.close()
+    #write results
     with open("outputs/primes_distributed.txt", "w") as f:
         for p in sorted(primes):
             f.write(f"{p}\n")
@@ -51,4 +60,5 @@ def coordinator_main():
     print("finished")
 
 if __name__ == "__main__":
+
     coordinator_main()
