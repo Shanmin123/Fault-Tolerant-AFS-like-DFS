9b62b430e5de00507cffdc9e7a981ae601e290cc
Jiaye39 <liujiaye39@gmail.com>
2025-11-01 01:15:11 +0000

    Implement send and receive data functions

---
+++ primefinder/send_recv.py
@@ -0,0 +1,13 @@
+def send_data(conn, data_part):
+    load=pickle.dumps(data_part)
+    data_len = len(load).to_bytes(8, 'big')
+    conn.sendall(data_len)
+    conn.sendall(load)
+
+def recv_data(conn):
+    data_len_bytes = conn.recv(8)
+    data_len = int.from_bytes(data_len_bytes, 'big')
+    data= b''
+    while len(data) < data_len:
+        data += conn.recv(4096)
+    return pickle.loads(data)
