992033424dc1982ff791afb9e330d5350debbf6b
Jiaye39 <liujiaye39@gmail.com>
2025-11-08 01:41:46 +0000

    attempt with heartbeat tracker
    Added worker management and heartbeat tracking to ensure task allocation and worker status monitoring.

--- primefinder/coordinator.py
+++ primefinder/coordinator.py
@@ -30,28 +30,53 @@ def coordinator_main():
 
     print(f"Coordinator started on {HOST}:{PORT}, waiting for {NUM_workers} workers...")
 
+    workers = {}
+    worker_id_counter = 0
+
+    task=[]
+    for i, chunk in enumerate(chunks):
+        task.append({'id':i, 'data':chunk, 'status': 'pending', 'used_worker':None})
+
+    # def_heartbeat_tracker
+    def heartbeat_tracker():
+        while True:
+            time.sleep(1)
+            now = time.time()
+            for wid,info in workers.items():
+                if info['alive'] and now - info['last_heartbeat'] < 10:
+                    print(f'Worker {wid} is dead')
+                    info['alive'] = False
+                    for t in task:
+                        if t['used_worker'] == wid and t['status'] != 'done':
+                            t['status'] = 'pending'
+                            t['used_worker'] = None
+    threading.Thread(target=heartbeat_tracker).start()
+
     #connect with worker
-    workers = []
-    while len(workers) < NUM_workers:
+     while len(workers) < NUM_workers:
         conn, addr = server.accept()
         print(f"Worker connected from {addr}")
-        workers.append(conn)
-        
-    #deliver data
-    for i, conn in enumerate(workers):
-        send_data(conn, chunks[i])
+        worker_id_counter+=1
+        wid=f'{worker_id_counter}'
+        workers[wid] = {'conn':conn,'addr':addr, 'alive':True, 'last_heartbeat':time.time()}
+        print(f'Worker {wid} connected from {addr}')
 
-    #receive results
-    for i, conn in enumerate(workers):
-        print(f'Receiving from worker{i + 1}')
-        try:
-            result= recv_data(conn)
-            if result:
-                primes.update(result)
-        except Exception as e:
-            print(f'Error receiving from worker{i+1}: {e}')
-        finally:
-            conn.close()
+    #deliver task
+    for i,(wid,info) in enumerate(workers.items()):
+        if i< len(task):
+            task=task[i]
+            send_data(info['conn'], {'type':'Task','data':task['data'],'task_id':task['task_id'],'status':task['id']})
+            task['status']='running'
+            task['used_worker'] = wid
+            print(f'Worker {wid} do {task['id']}')
+
+    ###Update latter 11.8  main loop_receive task and heart
+    completed=0
+    while completed<len(task):
+        
+   
+
+    
     #write results
     with open("outputs/primes_distributed.txt", "w") as f:
         for p in sorted(primes):
@@ -62,3 +87,4 @@ def coordinator_main():
 if __name__ == "__main__":
 
     coordinator_main()
+
