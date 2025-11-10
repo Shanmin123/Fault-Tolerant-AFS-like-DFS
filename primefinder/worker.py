f1227385901c0a4842d3a6b334fb11b4c0659088
Xinlu <lynnhvx@gmail.com>
2025-11-10 22:15:52 +0000

    Update worker.py
    1. 添加load——snapshot函数
    
            2.worker函数下：
            workerid需传参，numid从0改为从snapshot读
            while循环里给workerstate加了time

--- primefinder/worker.py
+++ primefinder/worker.py
@@ -1,22 +1,48 @@
 import socket
 import pickle
+import sys
+
 from prime import is_prime
 import select
 import time
+import os
 HOST = 'localhost'
 PORT = 5000
+snapshot_path = f"snapshots/snapshot_latest.pkl"
+
+def load_snapshot(workerid):
+    if not os.path.exists(snapshot_path):
+        return 0
+    f = open(snapshot_path, "rb")
+    snap = pickle.load(f)
+    f.close()
+    worker_state = snap.get("worker_state", {})
+    if workerid in worker_state:
+        numid = worker_state[workerid]["numid"]
+        print("worker is recovering", "worker", workerid, "starts from ", numid,"th number in the chunk")
+
+        return numid
+    return 0
 
 def worker():
+    workerid = int(sys.argv[1])
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((HOST, PORT))
-    print("Connected to coordinator")
+    print(f"woker{workerid} connected to coordinator")
+
+    numid = load_snapshot(workerid)
+    if numid != 0:
+
+        message = {"type": "reconnect", "workerid": workerid, "numid": numid}
+        server.sendall(pickle.dumps(message))
+        print("reconnect message is sent")
 
     data = server.recv(4096)
+
     task = pickle.loads(data)
     numbers = task["chunk"]
-    workerid = task["workerid"]
 
-    numid = 0
+
     found = []
     seen_marker = {}
 
@@ -24,14 +50,15 @@ def worker():
 
         readable, _, _ = select.select([server], [], [], 0)
         if server in readable:
-            ctrl_data = server.recv(4096)
-            if ctrl_data:
-                message = pickle.loads(ctrl_data)
-                if message.get("type") == "marker":
+            data = server.recv(4096)
+            if data:
+                message = pickle.loads(data)
+                if message["type"] == "marker":
                     sid = message["snapshot_id"]
-                    if not seen_marker.get(sid, False):
+                    if sid not in seen_marker:
                         seen_marker[sid] = True
                         state = {
+                            "time": time.time(),
 
                             "workerid": workerid,
                             "numid": numid,
