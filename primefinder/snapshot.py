7e70aa88d3846245e4acef4438239548786a4f3a
Xinlu <lynnhvx@gmail.com>
2025-11-10 23:12:33 +0000

    Update snapshot.py
    添加了线程锁

--- primefinder/snapshot.py
+++ primefinder/snapshot.py
@@ -1,6 +1,7 @@
 
 import pickle
 import time
+import threading
 
 
 snapshot_id = 0
@@ -11,48 +12,58 @@ inflight = {}
 worker_state = {}
 local_state = {}
 
+lock = threading.Lock()
+
 def init(n):
     global NUM_workers
-    NUM_workers = n
+    with lock:
+        NUM_workers = n
 
 def start(collected_count):
     global snapshot_id, active, marker_received, inflight, worker_state, local_state
-    if active == True:
-        return None
-    snapshot_id += 1
-    active = True
-    for i in range (NUM_workers):
-        marker_received[i+1] = False
-        inflight[i+1] = []
-    worker_state = {}
-    local_state = {"time": time.time(), "collected": collected_count}
-    print(f"start{snapshot_id} ")
-    return snapshot_id
+    with lock:
+        if active == True:
+            return None
+        snapshot_id += 1
+        active = True
+        for i in range(NUM_workers):
+            marker_received[i + 1] = False
+            inflight[i + 1] = []
+        worker_state = {}
+        local_state = {"time": time.time(), "collected": collected_count}
+        print(f"start{snapshot_id} ")
+        return snapshot_id
+
 
 def save_inflight(wokerid, prime):
-    if active and not marker_received.get(wokerid, False):
-        inflight[wokerid].append(prime)
+    with lock:
+        if active and not marker_received.get(wokerid, False):
+            inflight[wokerid].append(prime)
+
 
 def save_snapshot(workerid, sid, state):
     global active
-    if active and sid == snapshot_id and not marker_received[workerid]:
-        worker_state[workerid] = state
-        marker_received[workerid] = True
-        if all(marker_received.values()) == True:
-            data = {
-                "snapshot_id": snapshot_id,
-                "time": time.time(),
-                "coordinator_state": local_state,
-                "worker_state": worker_state,
-                "inflight": inflight
-            }
-            path1 = f"snapshots/snapshot_{snapshot_id}.pkl"
-            path2 = f"snapshots/snapshot_latest.pkl"
-            with open(path1, "wb") as f:
-                pickle.dump(data, f)
-            with open(path2, "wb") as f:
-                pickle.dump(data, f)
-            print(f"{snapshot_id}saved")
-            active = False
+    with lock:
+        if active and sid == snapshot_id and not marker_received[workerid]:
+            worker_state[workerid] = state
+            marker_received[workerid] = True
+            if all(marker_received.values()) == True:
+                data = {
+                    "snapshot_id": snapshot_id,
+                    "time": time.time(),
+                    "number of workers": NUM_workers,
+                    "coordinator_state": local_state,
+                    "worker_state": worker_state,
+                    "inflight": inflight
+                }
+                path1 = f"snapshots/snapshot_{snapshot_id}.pkl"
+                path2 = f"snapshots/snapshot_latest.pkl"
+                with open(path1, "wb") as f:
+                    pickle.dump(data, f)
+                with open(path2, "wb") as f:
+                    pickle.dump(data, f)
+                print(f"{snapshot_id}saved")
+                active = False
+
 
 
